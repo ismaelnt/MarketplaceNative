2acbfaf9682e90af934fccd24670c705f74534c0
ismaelnt <ismaelnt97@gmail.com>
2023-05-16 08:24:02 -0300

    feat: create module login

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import { Login } from './modules/login';
 
 export function App() {
   return (
     <SafeAreaView>
-      <Text>Olá Mundo</Text>
+      <Login />
     </SafeAreaView>
   );
 }
